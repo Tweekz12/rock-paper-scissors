1dcddb0e93194180a9871b31e48335b380a092cd
Tweekz <andreipetrea50@gmail.com>
2023-03-02 19:17:06 +0200

    Add getComputerChoice to return a random choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'Rock';
+    } else if (choice === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
