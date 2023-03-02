676563a8b6f51c64e1522772c293d8ab4dd44a2c
Tweekz <andreipetrea50@gmail.com>
2023-03-02 19:27:42 +0200

    Add function to compute the winner of a single round

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'Paper') {
+        return 'You Lose! Paper beats Rock';
+    } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'Scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'Rock') {
+        return 'You Win! Paper beats Rock';
+    } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'Scissors') {
+        return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'Rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'Paper') {
+        return 'You Win! Scissors beats Paper';
+    } else {
+        return "It's a Draw!";
+    }
+}
